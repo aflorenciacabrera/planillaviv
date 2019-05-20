4a58b54af5f5d77eeb93c42bfd7ed03c4952dab3
Florencia Cabrera <aflorenciacabrera@gmail.com>
2019-05-20 10:16:47 -0300

    act

--- resources/views/planilla/dato.blade.php
+++ resources/views/planilla/dato.blade.php
@@ -57,7 +57,7 @@
                             </div>
                             <div class="col-1">
                                 <input type="text" class="form-control" name="area" id="area" placeholder="" required>
-                                 <input class="form-control"  type="number"  name="personal" step="1" min="0" max="1000000" placeholder="N° ." required/>
+                                 {{-- <input class="form-control"  type="number"  name="personal" step="1" min="0" max="1000000" placeholder="N° ." required/> --}}
                             </div>
                             <div class="col-1">
                                 <label for="fraccion">Fracción:</label>
