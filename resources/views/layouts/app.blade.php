84eceba0e6837a958ebb8de0e8fafdffa3427f45
Florencia Cabrera <aflorenciacabrera@gmail.com>
2019-05-24 08:08:02 -0300

    app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,7 +94,7 @@
                                     </form>
                                 </div>
                             </li>
-                            @elseif(Auth::user()->hasRole('listador'))
+                            @elseif(Auth::user()->hasRole('ingresador'))
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
