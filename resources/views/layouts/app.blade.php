70a123a3d5301a90ec1eb941dc933a6000dc042e
Florencia Cabrera <aflorenciacabrera@gmail.com>
2019-05-24 08:04:26 -0300

    app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -100,11 +100,12 @@
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                 <a class="dropdown-item" href="{{ url('/planilla/verificar') }}">
-                                     Verificar Planillas
-                                </a>
+                                 
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('/planilla/verificar') }}">
+                                     Verificar Planillas
+                                </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
